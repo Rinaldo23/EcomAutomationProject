023fd62f65481ae6d692c0f5bbbcef8f09fb0c27
rinaldopankaj <rinaldo.pankaj@example.com>
2023-11-11 14:18:32 +0530

    BaseClass & Demo Test Run

---
+++ MyStore/src/main/java/com/mystore/base/BaseClass.java
@@ -0,0 +1,54 @@
+package com.mystore.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	
+	public static Properties prop;
+	public static WebDriver driver;
+	
+	public void loadConfig() {
+		try {
+			prop = new Properties();
+			FileInputStream ip = new FileInputStream(
+					System.getProperty("user.dir") + "\\Configuration\\config.properties");
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void launchApp() {
+		loadConfig();
+		WebDriverManager.chromedriver().setup();
+		String browserName = prop.getProperty("browser");
+		System.out.println(browserName);
+		
+		if (browserName.contains("Chrome")) {
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("FireFox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (browserName.equalsIgnoreCase("IE")) {
+			WebDriverManager.iedriver().setup();
+			driver = new InternetExplorerDriver();
+		}
+		
+		driver.get(prop.getProperty("url"));
+		
+	}
+	
+}
---
+++ MyStore/src/test/java/com/mystore/testcases/DemoRun.java
@@ -0,0 +1,15 @@
+package com.mystore.testcases;
+
+import com.mystore.base.BaseClass;
+
+public class DemoRun {
+
+	public static void main(String[] args) {
+		
+		BaseClass obj = new BaseClass();
+		obj.launchApp();
+		
+
+	}
+
+}
